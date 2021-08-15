86e7541aca1f20d9788fe621763b7d24954ab9dd
Vizards8 <1774836549@qq.om>
2021-08-15 23:31:20 +0800

    commit

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,8 @@ from Slice import Slice
 from hparam import hparams as hp
 import os
 
-source_train_dir = './dataset/train/source'
-label_train_dir = './dataset/train/label'
+source_train_dir = './dataset/train/source/'
+label_train_dir = './dataset/train/label/'
 
 if not os.path.exists(hp.source_train_dir):
     os.makedirs(hp.source_train_dir)
