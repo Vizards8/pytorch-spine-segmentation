5bb2981a7ff1824ccdc37120426af297c82f44f1
Vizards8 <1774836549@qq.om>
2021-08-17 11:03:51 +0800

    commit

--- data_function.py
+++ data_function.py
@@ -35,6 +35,7 @@ from torchio.transforms import (
 from pathlib import Path
 
 from hparam import hparams as hp
+from tqdm import tqdm
 
 
 class MedData_train(torch.utils.data.Dataset):
@@ -51,42 +52,83 @@ class MedData_train(torch.utils.data.Dataset):
         queue_length = 5
         samples_per_volume = 1
 
-        self.subjects = []
-
         images_dir = Path(images_dir)
         self.image_paths = sorted(images_dir.glob(hp.fold_arch))
         labels_dir = Path(labels_dir)
         self.label_paths = sorted(labels_dir.glob(hp.fold_arch))
 
+        val_size = int(hp.val_split * len(self.image_paths))
+        test_size = int(hp.test_split * len(self.image_paths))
+        train_size = len(self.image_paths) - val_size - test_size
+
+        self.subjects_paths = []  # dict打包路径，用于split
         for (image_path, label_path) in zip(self.image_paths, self.label_paths):
+            subject_path = {
+                'source': image_path,
+                'label': label_path,
+            }
+            self.subjects_paths.append(subject_path)
+
+        train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(
+            self.subjects_paths,
+            [train_size, val_size, test_size],
+            torch.Generator().manual_seed(0))
+
+        # split完成，train,val分别从各自dataset中的路径加载
+        print('Loading Dataset......\n')
+
+        self.train_dataset = self.load_from_dataset(train_dataset)
+        self.val_dataset = self.load_from_dataset(val_dataset)
+
+        # # 一次性加载所有data，内存开销巨大，口区
+        # self.subjects = []
+        #
+        # for (image_path, label_path) in zip(self.image_paths, self.label_paths):
+        #     subject = tio.Subject(
+        #         source=tio.ScalarImage(image_path),
+        #         label=tio.LabelMap(label_path),
+        #     )
+        #     torch.round(subject['label']['data'], out=subject['label']['data'])  # 处理为int
+        #
+        #     if not np.equal(subject['source']['affine'], subject['label']['affine']).all:
+        #         print("ERROR Handling:Affine not equal!", image_path.split('/')[-1])
+        #     else:
+        #         self.subjects.append(subject)
+        #
+        # self.transforms = self.transform()
+        #
+        # self.training_set = tio.SubjectsDataset(self.subjects, transform=self.transforms)
+        #
+        # self.queue_dataset = Queue(
+        #     self.training_set,
+        #     queue_length,
+        #     samples_per_volume,
+        #     UniformSampler(patch_size),
+        # )
+
+    def load_from_dataset(self, dataset):
+        # dataset 为list中嵌套字典，字典包含两个路径'source'与'label'
+        subjects = []
+
+        for i in tqdm(dataset):
             subject = tio.Subject(
-                source=tio.ScalarImage(image_path),
-                label=tio.LabelMap(label_path),
+                source=tio.ScalarImage(i['source']),
+                label=tio.LabelMap(i['label']),
             )
             torch.round(subject['label']['data'], out=subject['label']['data'])  # 处理为int
 
             if not np.equal(subject['source']['affine'], subject['label']['affine']).all:
                 print("ERROR Handling:Affine not equal!", image_path.split('/')[-1])
             else:
-                self.subjects.append(subject)
+                subjects.append(subject)
 
-        self.transforms = self.transform()
-
-        self.training_set = tio.SubjectsDataset(self.subjects, transform=self.transforms)
-
-        self.queue_dataset = Queue(
-            self.training_set,
-            queue_length,
-            samples_per_volume,
-            UniformSampler(patch_size),
-        )
+        return tio.SubjectsDataset(subjects, transform=self.transform())
 
     # def __init__(self, images_dir, labels_dir):
     #
     #     if hp.mode == '3d':
     #         patch_size = hp.patch_size
     #     elif hp.mode == '2d':
-    #         # patch_size = (hp.patch_size,hp.patch_size,1)
     #         patch_size = hp.patch_size
     #     else:
     #         raise Exception('no such kind of mode!')
