9c05cd234663e02e58ee75c9385cbe6aa2f22a55
Vizards8 <1774836549@qq.om>
2021-08-23 10:45:00 +0800

    modify

--- hparam.py
+++ hparam.py
@@ -8,9 +8,9 @@ class hparams:
     total_epochs = 200
     epochs_per_checkpoint = 10
     batch_size = 1
-    ckpt = None
-    val_split = 0.05
-    test_split = 0.75
+    ckpt = None  # 用来断点继续训练
+    val_split = 0.2
+    test_split = 0.99
     init_lr = 0.002
     scheduer_step_size = 20
     scheduer_gamma = 0.8
@@ -21,11 +21,12 @@ class hparams:
     out_classlist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18]
 
     use_queue = False
+    samples_per_volume = 5
     crop_or_pad_size = 880, 880, 1
     # if 2D: 256,256,1 if 3D: 256,256,256
-    patch_size = 880, 880, 1
+    patch_size = 512, 512, 1
     # if 2D: 128,128,1 if 3D: 128,128,128
-    num_workers = 2
+    num_workers = 0  # cpu电脑将此改为0，我觉得好像没啥区别
 
     # for test
     patch_overlap = 4, 4, 0  # if 3D: 4,4,4
