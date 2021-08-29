2c75d5b8af94893cf0971543eadf4ce3a0130519
Vizards8 <1774836549@qq.om>
2021-08-29 13:03:28 +0800

    all done

--- utils/metrics.py
+++ utils/metrics.py
@@ -188,6 +188,22 @@ def FP_FN_rate(pred, gt, eps=1e-5):
     return false_positive_rate.sum() / N, false_negtive_rate.sum() / N
 
 
+def pixel_accuracy(pred, gt, eps=1e-5):
+    """TP / (TP + FN)"""
+    N = gt.size(0)
+    pred_flat = pred.view(N, -1)
+    gt_flat = gt.view(N, -1)
+
+    tp = torch.sum((pred_flat != 0) * (gt_flat != 0), dim=1)
+    fn = torch.sum((pred_flat == 0) * (gt_flat != 0), dim=1)
+
+    score = (tp.float() + eps) / ((tp + fn).float() + eps)
+    # if score < 0.01:
+    #     print(
+    #         f'score:{score.item()}, gt:{torch.sum(gt_flat, dim=1).item()}, pred:{torch.sum(pred_flat, dim=1).item()}, tp:{tp.item()}, fn:{fn.item()}')
+    return score.sum() / N
+
+
 def diceCoeffv3(pred, gt, eps=1e-5):
     r""" computational formula：
         dice = (2 * tp) / (2 * tp + fp + fn)
@@ -285,22 +301,6 @@ def precision(pred, gt, eps=1e-5):
 
     score = (tp.float() + eps) / ((tp + fp).float() + eps)
 
-    return score.sum() / N
-
-
-def pixel_accuracy(pred, gt, eps=1e-5):
-    """TP / (TP + FN)"""
-    N = gt.size(0)
-    pred_flat = pred.view(N, -1)
-    gt_flat = gt.view(N, -1)
-
-    tp = torch.sum((pred_flat != 0) * (gt_flat != 0), dim=1)
-    fn = torch.sum((pred_flat == 0) * (gt_flat != 0), dim=1)
-
-    score = (tp.float() + eps) / ((tp + fn).float() + eps)
-    # if score < 0.01:
-    #     print(
-    #         f'score:{score.item()}, gt:{torch.sum(gt_flat, dim=1).item()}, pred:{torch.sum(pred_flat, dim=1).item()}, tp:{tp.item()}, fn:{fn.item()}')
     return score.sum() / N
 
 
